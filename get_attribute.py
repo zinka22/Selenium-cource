dcfee0e2ecd8fba6db4a62ac6d7703f8a0794fde
zinka22 <natasha-labchuk@mail.ru>
2024-04-08 20:49:19 +0300

    добавлены все решения задач с курса с ссылками на условие в файле READMI

---
+++ get_attribute.py
@@ -0,0 +1,38 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import math
+import time
+
+
+try:
+    link = "https://suninjuly.github.io/selects1.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    chest_element = browser.find_element(By.CSS_SELECTOR, "[src='images/chest.png']")
+    x = float(chest_element.get_attribute("valuex"))
+
+    result_function = math.log(abs(12 * math.sin(x)))
+
+    answer_field = browser.find_element(By.CSS_SELECTOR, "#answer")
+    answer_field.send_keys(result_function)
+
+    checkbox = browser.find_element(By.CSS_SELECTOR, "#robotCheckbox")
+    checkbox.click()
+
+    radio = browser.find_element(By.CSS_SELECTOR, "#robotsRule")
+    radio.click()
+
+    submit_button = browser.find_element(By.CSS_SELECTOR, "[class='btn btn-default']")
+    submit_button.click()
+
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+
+
+
---
+++ radio_and_checkbox.py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import math
+import time
+
+
+def calc(x):
+    return str(math.log(abs(12*math.sin(int(x)))))
+
+
+try:
+    link = "https://suninjuly.github.io/math.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    x_element = browser.find_element(By.CSS_SELECTOR, ".form-group #input_value")
+    x = x_element.text
+    y = calc(x)
+
+    input1 = browser.find_element(By.CSS_SELECTOR, "#answer.form-control")
+    input1.send_keys(y)
+
+    checkbox = browser.find_element(By.CSS_SELECTOR, "[for='robotCheckbox']")
+    checkbox.click()
+
+    radio = browser.find_element(By.CSS_SELECTOR, "[for='robotsRule']")
+    radio.click()
+
+    submit_button = browser.find_element(By.CSS_SELECTOR, "[class='btn btn-default']")
+    submit_button.click()
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+
+
+
