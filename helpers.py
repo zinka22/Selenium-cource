8f380f73eff5df2c46d04cc778af79710d976d96
zinka22 <natasha-labchuk@mail.ru>
2024-04-26 22:00:59 +0300

    lost commit

--- helpers.py
+++ helpers.py
@@ -2,4 +2,5 @@ from math import log, sin
 
 
 def calculate_value(x: int):
+    """this function helps to calculate math expression with the value of an argument"""
     return str(log(abs(12 * sin(x))))
