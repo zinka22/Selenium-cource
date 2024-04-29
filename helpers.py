df75a5742694980750de9c19d1cf9c7a2fc07a9c
zinka22 <natsha-labchuk@mail.ru>
2024-04-29 22:03:42 +0300

    new docstring

--- helpers.py
+++ helpers.py
@@ -2,7 +2,7 @@ from math import log, sin
 
 
 def solve_captcha(number: int):
-    """this function helps to calculate math expression,
-    witch value will be used to solve captcha
+    """Calculate math expression, which value
+    will be used to solve captcha
     """
     return str(log(abs(12 * sin(number))))
