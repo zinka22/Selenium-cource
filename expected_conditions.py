a40a786237afbe319c498edb0c3b70d77f56e70d
zinka22 <natasha-labchuk@mail.ru>
2024-05-27 13:36:41 +0300

    made low regisr to EC, used black to file

--- expected_conditions.py
+++ expected_conditions.py
@@ -2,7 +2,7 @@
 
 
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.ui import WebDriverWait
 
 import helpers
@@ -13,7 +13,7 @@ try:
         link="http://suninjuly.github.io/explicit_wait2.html"
     )
     check_for_correct_price = WebDriverWait(browser, 12).until(
-        EC.text_to_be_present_in_element((By.ID, "price"), "100")
+        ec.text_to_be_present_in_element((By.ID, "price"), "100")
     )
     button = browser.find_element(By.ID, "book")
     button.click()
@@ -29,4 +29,3 @@ try:
 
 finally:
     helpers.wait_ten_seconds_and_close(browser)
-
