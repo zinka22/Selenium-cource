ae0b289aa4d2dfd2d7603c570fccab633a7f7b94
Artjom Mashkov <ArtjomMashkov-job@yahoo.com>
2024-05-30 20:40:08 +0400

    runpy.run_path added

--- pytest_style.py
+++ pytest_style.py
@@ -1,20 +1,11 @@
 # Задание содержится в tasks/pytest_style.md
 
-
-import subprocess
-
-import pytest
+from runpy import run_path
 
 
 def test_check_registration_form_link1():
-    result = subprocess.run(["python", "check_registration_form.py"])
-
-    if result.returncode != 0:
-        pytest.fail()
+    run_path("check_registration_form.py")
 
 
 def test_check_registration_form_link2():
-    result = subprocess.run(["python", "check_registration_with_bugz.py"])
-
-    if result.returncode != 0:
-        pytest.fail()
+    run_path("check_registration_with_bugz.py")
