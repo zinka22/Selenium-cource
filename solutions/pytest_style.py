15d92e5dc1b6172578b7c79aed1ef3368c278455
Natali <108293161+zinka22@users.noreply.github.com>
2024-06-04 14:44:12 +0300

    Задача: запуск тестов в Pytest (#18)
    Various commits

---
+++ solutions/pytest_style.py
@@ -0,0 +1,11 @@
+# Задание содержится в tasks/pytest_style.md
+
+from runpy import run_path
+
+
+def test_check_registration_form():
+    run_path("check_registration_form.py")
+
+
+def test_check_registration_with_bugs():
+    run_path("check_registration_with_bugz.py")
