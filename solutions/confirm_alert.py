3e93c0d8d885765eb6608ab427d144024a6c81fa
zinka22 <natasha-labchuk@mail.ru>
2024-06-05 20:53:49 +0300

    refactored confirm_alert

--- solutions/confirm_alert.py
+++ solutions/confirm_alert.py
@@ -16,12 +16,11 @@ try:
     confirm = browser.switch_to.alert
     confirm.accept()
 
-    find_element_x = browser.find_element(By.ID, "input_value")
-    element_x_int = int(find_element_x.text)
-    math_result = helpers.solve_math_expression_for_captcha(element_x_int)
+    input_value = int(browser.find_element(By.ID, "input_value").text)
+    function_value = helpers.solve_math_expression_for_captcha(input_value)
 
     answer_form = browser.find_element(By.ID, "answer")
-    answer_form.send_keys(math_result)
+    answer_form.send_keys(function_value)
 
     submit_button = browser.find_element(By.CLASS_NAME, "btn-primary")
     submit_button.click()
