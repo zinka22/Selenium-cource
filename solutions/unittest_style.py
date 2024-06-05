cf39c3512a5308d09db6020167add3f8298c2280
zinka22 <natasha-labchuk@mail.ru>
2024-06-05 14:14:21 +0300

    delete block try/finally(2nd part)

--- solutions/unittest_style.py
+++ solutions/unittest_style.py
@@ -12,82 +12,76 @@ class TestAbs(unittest.TestCase):
     def test_check_registration_form(self):
 
         browser = None
-            browser = helpers.open_browser_page(
-                link="http://suninjuly.github.io/registration1.html"
-            )
+        browser = helpers.open_browser_page(
+            link="http://suninjuly.github.io/registration1.html"
+        )
 
-            # Ваш код, который заполняет обязательные поля
-            input1 = browser.find_element(By.CLASS_NAME, "form-control.first")
-            input1.send_keys("Ivan")
-            input2 = browser.find_element(By.CLASS_NAME, "form-control.second")
-            input2.send_keys("Petrov")
-            input3 = browser.find_element(By.CLASS_NAME, "form-control.third")
-            input3.send_keys("nn@mailto.plus")
+        # Ваш код, который заполняет обязательные поля
+        input1 = browser.find_element(By.CLASS_NAME, "form-control.first")
+        input1.send_keys("Ivan")
+        input2 = browser.find_element(By.CLASS_NAME, "form-control.second")
+        input2.send_keys("Petrov")
+        input3 = browser.find_element(By.CLASS_NAME, "form-control.third")
+        input3.send_keys("nn@mailto.plus")
 
-            # Отправляем заполненную форму
-            button = browser.find_element(By.CSS_SELECTOR, "button.btn")
-            button.click()
+        # Отправляем заполненную форму
+        button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+        button.click()
 
-            # Проверяем, что смогли зарегистрироваться
-            # ждем загрузки страницы
-            sleep(1)
+        # Проверяем, что смогли зарегистрироваться
+        # ждем загрузки страницы
+        sleep(1)
 
-            # находим элемент, содержащий текст
-            welcome_text_element = browser.find_element(By.TAG_NAME, "h1")
-            # записываем в переменную welcome_text текст из элемента welcome_text_element
-            welcome_text = welcome_text_element.text
+        # находим элемент, содержащий текст
+        welcome_text_element = browser.find_element(By.TAG_NAME, "h1")
+        # записываем в переменную welcome_text текст из элемента welcome_text_element
+        welcome_text = welcome_text_element.text
 
-            self.assertEqual(
-                "Congratulations! You have successfully registered!",
-                welcome_text,
-                "Welcome text should be 'Congratulations! You have successfully registered!'",
-            )
-
-        finally:
-            helpers.wait_ten_seconds_and_close(browser)
+        self.assertEqual(
+            "Congratulations! You have successfully registered!",
+            welcome_text,
+            "Welcome text should be 'Congratulations! You have successfully registered!'",
+        )
 
     def test_check_registration_with_bugs(self):
         browser = None
-            browser = helpers.open_browser_page(
-                link="http://suninjuly.github.io/registration2.html"
-            )
+        browser = helpers.open_browser_page(
+            link="http://suninjuly.github.io/registration2.html"
+        )
 
-            # Ваш код, который заполняет обязательные поля
-            input1 = browser.find_element(
-                By.CSS_SELECTOR, '[class="form-control first"]:required'
-            )
-            input1.send_keys("Ivan")
-            input2 = browser.find_element(
-                By.CSS_SELECTOR, '[class="form-control second"]:required'
-            )
-            input2.send_keys("Petrov")
-            input3 = browser.find_element(
-                By.CSS_SELECTOR, '[class="form-control third"]:required'
-            )
-            input3.send_keys("nn@mailto.plus")
+        # Ваш код, который заполняет обязательные поля
+        input1 = browser.find_element(
+            By.CSS_SELECTOR, '[class="form-control first"]:required'
+        )
+        input1.send_keys("Ivan")
+        input2 = browser.find_element(
+            By.CSS_SELECTOR, '[class="form-control second"]:required'
+        )
+        input2.send_keys("Petrov")
+        input3 = browser.find_element(
+            By.CSS_SELECTOR, '[class="form-control third"]:required'
+        )
+        input3.send_keys("nn@mailto.plus")
 
-            # Отправляем заполненную форму
-            button = browser.find_element(By.CSS_SELECTOR, "button.btn")
-            button.click()
+        # Отправляем заполненную форму
+        button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+        button.click()
 
-            # Проверяем, что смогли зарегистрироваться
-            # ждем загрузки страницы
-            sleep(1)
+        # Проверяем, что смогли зарегистрироваться
+        # ждем загрузки страницы
+        sleep(1)
 
-            # находим элемент, содержащий текст
-            welcome_text_element = browser.find_element(By.TAG_NAME, "h1")
-            # записываем в переменную welcome_text текст из элемента welcome_text_element
-            welcome_text = welcome_text_element.text
+        # находим элемент, содержащий текст
+        welcome_text_element = browser.find_element(By.TAG_NAME, "h1")
+        # записываем в переменную welcome_text текст из элемента welcome_text_element
+        welcome_text = welcome_text_element.text
 
-            # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
-            self.assertEqual(
-                "Congratulations! You have successfully registered!",
-                welcome_text,
-                "Welcome text should be 'Congratulations! You have successfully registered!'",
-            )
-
-        finally:
-            helpers.wait_ten_seconds_and_close(browser)
+        # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+        self.assertEqual(
+            "Congratulations! You have successfully registered!",
+            welcome_text,
+            "Welcome text should be 'Congratulations! You have successfully registered!'",
+        )
 
 
 if __name__ == "__main__":
