b04e41c4b3ad36b01ffa524aeea58771345fddaf
Natali <108293161+zinka22@users.noreply.github.com>
2024-06-07 12:26:12 +0300

    Перенос файла с заданием: fill the huge form (#28)
    Various commits

---
+++ solutions/fill_the_huge_form.py
@@ -0,0 +1,21 @@
+# Задание содержится в tasks/fill_the_huge_form.md
+
+from selenium.webdriver.common.by import By
+
+import helpers
+
+browser = None
+try:
+    browser = helpers.open_browser_page(
+        link="http://suninjuly.github.io/huge_form.html"
+    )
+
+    text_fields = browser.find_elements(By.TAG_NAME, "input")
+    for field in text_fields:
+        field.send_keys("Наташа Умница")
+
+    submit_button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    submit_button.click()
+
+finally:
+    helpers.wait_ten_seconds_and_close(browser)
