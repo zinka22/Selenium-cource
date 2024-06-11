0242530c5dadba49d42f48d994db5cefa66fb109
zinka22 <natasha-labchuk@mail.ru>
2024-06-11 22:46:35 +0300

    rename variables in py file

--- solutions/radio_and_checkbox.py
+++ solutions/radio_and_checkbox.py
@@ -11,14 +11,14 @@ try:
     input_value = browser.find_element(By.CSS_SELECTOR, ".form-group #input_value").text
     function_value = helpers.get_math_function_value(input_value)
 
-    input1 = browser.find_element(By.CSS_SELECTOR, "#answer.form-control")
-    input1.send_keys(function_value)
+    input_answer = browser.find_element(By.CSS_SELECTOR, "#answer.form-control")
+    input_answer.send_keys(function_value)
 
     checkbox = browser.find_element(By.CSS_SELECTOR, "[for='robotCheckbox']")
     checkbox.click()
 
-    radio = browser.find_element(By.CSS_SELECTOR, "[for='robotsRule']")
-    radio.click()
+    radio_button = browser.find_element(By.CSS_SELECTOR, "[for='robotsRule']")
+    radio_button.click()
 
     submit_button = browser.find_element(By.CSS_SELECTOR, "[class='btn btn-default']")
     submit_button.click()
