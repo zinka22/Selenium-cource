fd4dbf2396c4dd23e1d4fbbebf96bae96bdbe514
zinka22 <natasha-labchuk@mail.ru>
2024-06-13 23:11:41 +0300

    delete debug lines from conftest file

--- solutions/pytest_folder/conftest.py
+++ solutions/pytest_folder/conftest.py
@@ -5,11 +5,9 @@ import json
 
 @pytest.fixture(scope="function")
 def browser():
-    print("\nstart browser for test..")
     browser = webdriver.Chrome()
     browser.implicitly_wait(5)
     yield browser
-    print("\nquit browser..")
     browser.quit()
 
 
