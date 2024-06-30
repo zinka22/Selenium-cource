b00763b6a4b8ae397a3a3f71a5f8a7f1208184dd
Natali <108293161+zinka22@users.noreply.github.com>
2024-06-30 19:06:00 +0300

    Apply suggestions from code review
    Co-authored-by: Artjom <ArtjomMashkov-job@yahoo.com>

--- helpers.py
+++ helpers.py
@@ -30,14 +30,14 @@ def get_math_function_value(x: int | str) -> str:
 
 
 def open_browser_page(link):
-    """Call Chrome browser, then open link in browser."""
+    """Create Chrome browser instance, then open link in browser."""
     browser = webdriver.Chrome()
     browser.get(link)
     return browser
 
 
 def wait_ten_seconds_and_close(browser):
-    """Make wait for ten seconds, then quit browser with opened page/pages."""
+    """Explicit wait for 10 seconds, then quit browser with opened page/pages."""
     if browser:
         time.sleep(10)
         browser.quit()
