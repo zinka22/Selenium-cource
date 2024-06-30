8b74514971bab1b143f3c4fe16ef395eefd7f2fb
zinka22 <natasha-labchuk@mail.ru>
2024-06-30 22:29:02 +0300

    add logic to conftest and tet_run_for_different locals

---
+++ solutions/pytest_folder/test_run_for_different_locals.py
@@ -0,0 +1,9 @@
+from selenium.webdriver.common.by import By
+
+
+def test_run_for_different_locals(browser):
+    browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
+
+    add_to_basket_button = browser.find_element(By.CLASS_NAME, "btn-add-to-basket")
+
+    assert add_to_basket_button, "Add to basket button is absent"
