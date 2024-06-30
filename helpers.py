ee56b933ff90dbcc171f67a9349f568f9b50e51d
Artjom <ArtjomMashkov-job@yahoo.com>
2024-06-30 21:11:09 +0400

    Login verification improvement (#46)
    * Added waiting for presence of profile image & textarea and tests updated
    
    Resolves #44
    
    * Redundant code in test_parametrization.py removed

--- helpers.py
+++ helpers.py
@@ -10,18 +10,26 @@ from selenium.webdriver.support.ui import WebDriverWait
 base_url = "https://suninjuly.github.io"
 
 
-def assert_if_user_is_authorized(browser):
+def assert_if_user_is_authorized_on_lesson_page(browser):
     """Assert function to verify user authorization. Raise an assertion error if the user is not authorized."""
-    check_if_user_is_authorized = None
+    is_authorized_user = False
     if browser:
         try:
             WebDriverWait(browser, 5).until(
                 ec.invisibility_of_element_located((By.ID, "login_form"))
             )
-            check_if_user_is_authorized = True
+            WebDriverWait(browser, 5).until(
+                ec.presence_of_element_located((By.CLASS_NAME, "navbar__profile-img"))
+            )
+            WebDriverWait(browser, 5).until(
+                ec.presence_of_element_located(
+                    (By.XPATH, "//*[@data-type='string-quiz']")
+                )
+            )
+            is_authorized_user = True
         except TimeoutException:
-            check_if_user_is_authorized = False
-    assert check_if_user_is_authorized, "User is guest, the answer can't be sent"
+            ...
+    assert is_authorized_user, "User is guest, the answer can't be sent"
 
 
 def get_math_function_value(x: int | str) -> str:
