7f22f9374f5bb84b7d3f25ca7544be775f7a7760
Natali <108293161+zinka22@users.noreply.github.com>
2024-07-01 14:26:56 +0300

    Задача: Запуск автотестов для разных языков интерфейса(task_run_for_different_locals) (#47)
    Various commits

---
+++ solutions/pytest_folder/test_run_for_different_locals.py
@@ -0,0 +1,13 @@
+# Задание содержится в tasks/test_run_for_different_locals.md
+
+from time import sleep
+
+from selenium.webdriver.common.by import By
+
+
+def test_run_for_different_locals(browser):
+    browser.get("https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
+    sleep(30)
+    add_to_basket_button = browser.find_element(By.CLASS_NAME, "btn-add-to-basket")
+
+    assert add_to_basket_button, "Add to basket button is absent"
