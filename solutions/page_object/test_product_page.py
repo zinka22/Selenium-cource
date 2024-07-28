721c288691da176b6e2e91604a5cf1f6ce1c1055
zinka22 <natasha-labchuk@mail.ru>
2024-07-28 23:17:48 +0300

    add three negative tests in accordance with the task, commented implicit waits for checking tests (in accordance with the task too),clarify locator to success message

--- solutions/page_object/test_product_page.py
+++ solutions/page_object/test_product_page.py
@@ -1,5 +1,7 @@
 # Задание содержится в tasks/test_product_page.md
 
+import pytest
+
 from pages.product_page import ProductPage
 
 
@@ -11,3 +13,30 @@ def test_guest_can_add_product_to_basket(browser):
     page.add_product_to_cart()
     page.solve_quiz_and_get_code()
     page.should_be_in_cart()
+
+
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_cart()
+    page.solve_quiz_and_get_code()
+    page.should_not_be_success_message()
+
+
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
+
+
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_cart()
+    page.solve_quiz_and_get_code()
+    page.should_element_disappear()
