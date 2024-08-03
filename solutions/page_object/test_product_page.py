da3a703428840a470c30b679399a7e4905380db8
zinka22 <natasha-labchuk@mail.ru>
2024-08-03 21:49:20 +0300

    add variable with link to conftest file, add variable ro test_product_page and delete repeted link from it

--- solutions/page_object/test_product_page.py
+++ solutions/page_object/test_product_page.py
@@ -2,13 +2,12 @@
 # Задание к негативным тестам содержится в tasks/task_negative_tests.md
 
 import pytest
-
+from conftest import base_shop_url
 from pages.product_page import ProductPage
 
 
 def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    page = ProductPage(browser, link)
+    page = ProductPage(browser, base_shop_url)
     page.open()
     page.should_be_able_to_add_product_to_cart()
     page.add_product_to_cart()
@@ -18,8 +17,7 @@ def test_guest_can_add_product_to_basket(browser):
 
 @pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    page = ProductPage(browser, link)
+    page = ProductPage(browser, base_shop_url)
     page.open()
     page.add_product_to_cart()
     page.solve_quiz_and_get_code()
@@ -27,16 +25,14 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
 
 
 def test_guest_cant_see_success_message(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    page = ProductPage(browser, link)
+    page = ProductPage(browser, base_shop_url)
     page.open()
     page.should_not_be_success_message()
 
 
 @pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    page = ProductPage(browser, link)
+    page = ProductPage(browser, base_shop_url)
     page.open()
     page.add_product_to_cart()
     page.solve_quiz_and_get_code()
