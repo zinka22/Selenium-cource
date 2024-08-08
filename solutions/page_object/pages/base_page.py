ddc796d4e1c505bc9842623af242294c23d088c8
zinka22 <natasha-labchuk@mail.ru>
2024-08-08 12:57:03 +0300

    delete # from imlicitly wait in base_page

--- solutions/page_object/pages/base_page.py
+++ solutions/page_object/pages/base_page.py
@@ -11,7 +11,7 @@ class BasePage:
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
-        # self.browser.implicitly_wait(timeout)
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
