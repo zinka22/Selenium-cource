00587170102dcdbe7184ffcf9527ef3e686a444b
zinka22 <natasha-labchuk@mail.ru>
2024-08-19 21:21:18 +0300

    change default timeout in test_product_page

--- solutions/page_object/test_product_page.py
+++ solutions/page_object/test_product_page.py
@@ -2,6 +2,7 @@
 # Задание к негативным тестам содержится в tasks/task_negative_tests.md
 
 import pytest
+
 from pages.product_page import ProductPage
 
 base_shop_url = "https://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
@@ -27,14 +28,14 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
 
 
 def test_guest_cant_see_success_message(browser):
-    page = ProductPage(browser, base_shop_url, timeout=0)
+    page = ProductPage(browser, base_shop_url, timeout=implicit_wait_default_value)
     page.open()
     page.should_not_be_success_message()
 
 
 @pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser):
-    page = ProductPage(browser, base_shop_url, timeout=0)
+    page = ProductPage(browser, base_shop_url, timeout=implicit_wait_default_value)
     page.open()
     page.add_product_to_cart()
     page.solve_quiz_and_get_code()
