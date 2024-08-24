bf672e607bcbae6ed937d9cb90325b94d181cee9
zinka22 <natasha-labchuk@mail.ru>
2024-08-24 17:28:19 +0300

    add right locator to base_page, delete 1 line in main_page.py which is not used

--- solutions/page_object/pages/base_page.py
+++ solutions/page_object/pages/base_page.py
@@ -62,7 +62,7 @@ class BasePage:
             )
 
     def go_to_login_page(self):
-        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK_INVALID)
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
         link.click()
 
     def should_be_login_link(self):
