4321b0e0d0a7dc72e7fc1220c1aa47bba8f2b11a
zinka22 <natasha-labchuk@mail.ru>
2024-10-02 22:54:50 +0300

    sort names of variables in locators.py

--- solutions/page_object/pages/locators.py
+++ solutions/page_object/pages/locators.py
@@ -7,18 +7,18 @@ class BasePageLocators:
 
 
 class LoginPageLocators:
-    LOGIN_FORM = (By.ID, "id_login-username")
-    REGISTER_FORM = (By.ID, "id_registration-email")
-    PASSWORD_FORM = (By.ID, "id_registration-password1")
     CONFIRM_PASSWORD = (By.ID, "id_registration-password2")
     CONFIRM_REGISTRATION = (By.NAME, "registration_submit")
+    LOGIN_FORM = (By.ID, "id_login-username")
+    PASSWORD_FORM = (By.ID, "id_registration-password1")
+    REGISTER_FORM = (By.ID, "id_registration-email")
     SUCCESS_REGISTER = (By.CLASS_NAME, "alertinner")
 
 
 class ProductPageLocators:
     ADD_TO_CART = (By.CLASS_NAME, "btn-add-to-basket")
+    CART_PRICE = (By.CSS_SELECTOR, ".alertinner p strong")
     PRODUCT_NAME = (By.CLASS_NAME, "product_main h1")
     PRODUCT_NAME_IN_SUCCESS_MESSAGE = (By.CSS_SELECTOR, ".alertinner strong")
-    CART_PRICE = (By.CSS_SELECTOR, ".alertinner p strong")
     PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
     SUCCESS_MESSAGE = (By.CSS_SELECTOR, ".alert-success")
