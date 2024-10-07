62fd53bc7c7b2e3575b1b4afe964b4786a24a3bc
zinka22 <natasha-labchuk@mail.ru>
2024-10-07 21:08:22 +0300

    move function register new user(login_page) to the beginning of the class

--- solutions/page_object/pages/login_page.py
+++ solutions/page_object/pages/login_page.py
@@ -5,6 +5,23 @@ from .locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
+    def register_new_user(self):
+        email = str(time.time()) + "@fakemail.org"
+        register_form = self.browser.find_element(*LoginPageLocators.REGISTER_FORM)
+        register_form.send_keys(email)
+
+        password_form = self.browser.find_element(*LoginPageLocators.PASSWORD_FORM)
+        password_form.send_keys("2q3w4e5r6t7y")
+        confirm_password_form = self.browser.find_element(
+            *LoginPageLocators.CONFIRM_PASSWORD
+        )
+        confirm_password_form.send_keys("2q3w4e5r6t7y")
+
+        confirm_registration = self.browser.find_element(
+            *LoginPageLocators.CONFIRM_REGISTRATION
+        )
+        confirm_registration.click()
+
     def should_be_login_page(self):
         self.should_be_login_url()
         self.should_be_login_form()
@@ -23,20 +40,3 @@ class LoginPage(BasePage):
     def should_be_register_form(self):
         register_form = self.browser.find_element(*LoginPageLocators.REGISTER_FORM)
         assert register_form, "Register form is absent"
-
-    def register_new_user(self):
-        email = str(time.time()) + "@fakemail.org"
-        register_form = self.browser.find_element(*LoginPageLocators.REGISTER_FORM)
-        register_form.send_keys(email)
-
-        password_form = self.browser.find_element(*LoginPageLocators.PASSWORD_FORM)
-        password_form.send_keys("2q3w4e5r6t7y")
-        confirm_password_form = self.browser.find_element(
-            *LoginPageLocators.CONFIRM_PASSWORD
-        )
-        confirm_password_form.send_keys("2q3w4e5r6t7y")
-
-        confirm_registration = self.browser.find_element(
-            *LoginPageLocators.CONFIRM_REGISTRATION
-        )
-        confirm_registration.click()
